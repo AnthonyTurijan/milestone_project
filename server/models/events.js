24edc7717b96f0c373935604a76dcec4525e2a6e
AnthonyTurijan <anthonyturijan@gmail.com>
2022-06-03 18:25:52 -0700

    Adding Migrations

--- server/models/events.js
+++ server/models/events.js
@@ -9,8 +9,22 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({Users, Profiles, Comments}) {
+      //user
+      Events.belongsTo(Users,{
+        foreignKey: 'user_name',
+        as: 'users'
+      })
+      //Profiles
+      Events.belongsTo(Profiles,{
+        foreignKey:'profile_id',
+        as: 'profiles'
+      })
+      //Comments
+      Events.hasMany(Comments,{
+        foreignKey: 'event_id',
+        as: 'events '
+      })
     }
   }
   Events.init({
--- server/models/users.js
+++ server/models/users.js
@@ -9,8 +9,17 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({Events, Profiles}) {
+      //events
+      Users.hasMany(Events,{
+        foreignKey: 'user_name',
+        as: 'events'
+      })
+      //profile
+      Users.hasOne(Profiles,{
+        foreignKey: 'user_id',
+        as: 'profiles'
+      })
     }
   }
   Users.init({
